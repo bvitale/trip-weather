b7bdbc8164b9363c89c1457200922ca2b65b492e
Ben Vitale <ben.vitale@ellucian.com>
2018-03-16 10:12:17 -0400

    Fix UI viewport on mobile. Add more stops. Ignore past dates; ignore dates beyond 7 days

--- src/app.js
+++ src/app.js
@@ -16,12 +16,21 @@ async function getPredictions() {
       summary: `No prediction yet`
     };
     const stopDate = moment(stop.date);
+    if (stopDate.diff(moment(), 'days') < 0) {
+      console.log(`skipping ${stopDate.format()}, date is past`);
+      continue;
+    }
+    if (stopDate.diff(moment(), 'days') >= 7) {
+      console.log(`skipping ${stopDate.format()}, date is more than 7 days out`);
+      results.push(prediction);
+      continue;
+    }
+    console.log(`Requesting prediction for ${prediction.location} on ${prediction.date}`);
     const options = {
       uri: `https://api.darksky.net/forecast/${process.env.API_KEY}/${stop.gps}?exclude=currently,hourly,minutely`,
       json: true
     };
     const response = await request(options);
-    console.log(JSON.stringify(response, null, 2));
     response.daily.data.forEach(day => {
       const date = moment.unix(day.time);
       if (date.isSame(stopDate, 'day')) {
